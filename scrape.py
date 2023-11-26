e1770ced91b01677380b64713709b8e0b662651c
velocitatem <daniel@alves.world>
2023-11-26 18:44:55 +0100

    definitions for methods

--- scrape.py
+++ scrape.py
@@ -4,6 +4,8 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import pandas as pd
+import nltk
+from nltk import word_tokenize, pos_tag, ne_chunk
 
 url = 'https://www.ie.edu/'
 r = requests.get(url)
@@ -14,13 +16,29 @@ body = soup.find('body')
 spans = body.find_all('span')
 two_word_spans = [ span for span in spans if len(span.text.split()) == 2 ]
 
-import nltk
-from nltk import word_tokenize, pos_tag, ne_chunk
 
 nltk.download('averaged_perceptron_tagger')
 nltk.download('maxent_ne_chunker')
 nltk.download('words')
 
+
+def scrape_website_content(url):
+    """
+    Function to scrape website content. Returns a list of 2 word string.
+    """
+    pass
+
+def get_names(string):
+    """
+    Function to get names from a string. Returns a list of names.
+    """
+    pass
+
+def get_links_from_url(url):
+    """
+    Function to get all links from a url. Returns a list of links.
+    """
+    pass
 
 def is_name(string):
     # Tokenize and POS tag the text
@@ -42,6 +60,9 @@ print(is_name(string))
 
 # get all names
 names = [ span.text for span in two_word_spans if is_name(span.text) ]
+
+# TODO Here define a crawler
+# goes through all the links and get all the names
 
 
 # get all emails
