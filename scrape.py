77e786f8ad6d1c9735dc5d5f203f4b6cb3c834a6
velocitatem <daniel@alves.world>
2023-12-03 01:38:19 +0100

    new model for scraping

--- scrape.py
+++ scrape.py
@@ -4,8 +4,6 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import pandas as pd
-import nltk
-from nltk import word_tokenize, pos_tag, ne_chunk
 
 url = 'https://www.ie.edu/'
 r = requests.get(url)
@@ -16,11 +14,6 @@ body = soup.find('body')
 spans = body.find_all('span')
 two_word_spans = [ span for span in spans if len(span.text.split()) == 2 ]
 
-
-nltk.download('averaged_perceptron_tagger')
-nltk.download('maxent_ne_chunker')
-nltk.download('words')
-nltk.download('punkt')
 
 
 def scrape_website_content(url):
@@ -33,7 +26,7 @@ def get_names(spans):
     """
     Function to get names from a string. Returns a list of names.
     """
-    return[ span.text.strip() for span in spans if is_name(span.text) ]
+    return[ span.text.strip() for span in spans if is_name(span.text.strip()) ]
 
 def get_links_from_url(page_contents):
     if page_contents is None:
@@ -43,42 +36,56 @@ def get_links_from_url(page_contents):
     lnks = [link for link in lnks if link.get('href') is not None]
     return [link.get('href') for link in lnks  if 'ie.edu' in link.get('href')]
 
+
+# Create a NER pipeline with the pre-trained model
+
+# mdarhri00/named-entity-recognition
+from transformers import pipeline
+
+pipe = pipeline("token-classification", model="mdarhri00/named-entity-recognition")
+
+from functools import cache
+
+@cache
 def is_name(string):
-    # Tokenize and POS tag the text
-    tokens = word_tokenize(string)
-    tags = pos_tag(tokens)
 
-    # Chunk named entities
-    tree = ne_chunk(tags)
-    exclusion = ['department', "degree", 'campus', 'legal']
-    if any(x in string.lower() for x in exclusion):
-        return False
-
-    # Check if any named entity is labeled as PERSON
-    return any(chunk.label() == 'PERSON' for chunk in tree if hasattr(chunk, 'label'))
-
+    # run the model
+    result = pipe(string)
+    r=[a['word'] for a in result if a['entity'] == "Person_Name"]
+    bl = len(r) > 0
+    return bl
 
 # get all names
 names = set([ span.text for span in two_word_spans if is_name(span.text) ])
 
+
 links = get_links_from_url(body)
+seen = set(links)
 # get all names
-limit = 100
+limit = 1000
 while len(links) > 0 and limit > 0:
     limit -= 1
     try:
         link = links.pop(0)
-        print(link, names)
         page = scrape_website_content(link)
         if page is None:
             continue
         # two word spans
-        spans = [ span for span in page.find_all('span') if len(span.text.split()) == 2 ]
+        # get all spans with more than one word
+        spans = [span for span in page.find_all('span') if len(span.text.split()) < 5]
+        print(len(spans))
+
         new_names = get_names(spans)
+        print(link, len(links), len(names), len(new_names))
         # add all new names
         [ names.add(name) for name in new_names]
-        links_on_link = get_links_from_url(page)
+        links_on_link = [a for a in get_links_from_url(page) if a not in seen]
+        for a in links_on_link:
+            seen.add(a)
         links.extend(links_on_link)
+        # save the names to a file
+        df = pd.DataFrame(names, columns=['name'])
+        df.to_csv('names.csv')
     except:
         pass
 
