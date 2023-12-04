38331a9f98ec69fac39cf1918238686a82c68610
velocitatem <daniel@alves.world>
2023-12-04 19:45:03 +0100

    cleaning and comments

--- money/models.py
+++ money/models.py
@@ -1,15 +1,19 @@
+"""
+Did not use foreign keys because using filler with the Django ORM is a pain.
+"""
 from django.db import models
 import uuid
 
 # Create your models here.
 class User(models.Model):
-    user_id = models.CharField(max_length=50, primary_key=True, unique=True, default=uuid.uuid4().hex[:50])
+    user_id = models.CharField(max_length=50, primary_key=True, unique=True, default=uuid.uuid4().hex[:50]) # default
+    # ... for primary key
     subscription_id = models.CharField(max_length=50)
-    name = models.CharField(max_length=255)  # Example for Name
-    surname = models.CharField(max_length=255)  # Example for Surname
-    email = models.EmailField()  # Example for Email
-    phone_number = models.CharField(max_length=20)  # Example for PhoneNumber
-    age = models.IntegerField()  # Example for Age
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    age = models.IntegerField()
     budget_id = models.CharField(max_length=50)
 
 class Budget(models.Model):
@@ -29,6 +33,7 @@ class Subscription(models.Model):
     subscription_start_date = models.DateField()
     subscription_end_date = models.DateField()
 
+# ExpenseType, Expense
 class ExpenseType(models.Model):
     expense_type_id = models.CharField(max_length=50, primary_key=True, unique=True, default=uuid.uuid4().hex[:50])
     expense_type_name = models.CharField(max_length=50)
@@ -40,7 +45,7 @@ class Expense(models.Model):
     transaction_id = models.CharField(max_length=50)
 
 # make use of mongodb for transactions
-class Transaction(models.Model):
+class Transaction(models.Model): # our model for mongo unstruct
     transaction_id = models.CharField(max_length=50, primary_key=True)
     transaction_amount = models.FloatField()
     transaction_date = models.DateField()
--- student_finances/settings.py
+++ student_finances/settings.py
@@ -75,6 +75,7 @@ WSGI_APPLICATION = "student_finances.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # using sqlite for ease of use and replication
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
